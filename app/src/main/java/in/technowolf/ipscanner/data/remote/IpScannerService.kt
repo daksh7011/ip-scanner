23ab0f655c70fce7cdc28de06759f861ede9eadf
Daksh <daksh@technowolf.in>
2022-05-11 23:01:41 +0530

    Fix crash with retrofit and socket timeout exception.
    Signed-off-by: Daksh <daksh@technowolf.in>

--- app/src/main/java/in/technowolf/ipscanner/data/remote/IpScannerService.kt
+++ app/src/main/java/in/technowolf/ipscanner/data/remote/IpScannerService.kt
@@ -1,9 +1,10 @@
 package `in`.technowolf.ipscanner.data.remote
 
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface IpScannerService {
     @GET("ip/{ip}")
-    suspend fun getIpDetails(@Path("ip") ipAddress: String): IpDetailRS
+    suspend fun getIpDetails(@Path("ip") ipAddress: String): Response<IpDetailRS>
 }
--- app/src/main/java/in/technowolf/ipscanner/ui/home/HomeViewModel.kt
+++ app/src/main/java/in/technowolf/ipscanner/ui/home/HomeViewModel.kt
@@ -5,9 +5,11 @@ import `in`.technowolf.ipscanner.data.remote.IpDetailRS
 import `in`.technowolf.ipscanner.data.remote.IpScannerService
 import `in`.technowolf.ipscanner.data.remote.PublicIpService
 import `in`.technowolf.ipscanner.utils.Extensions.readOnly
+import `in`.technowolf.ipscanner.utils.safeCall
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class HomeViewModel(
@@ -16,7 +18,7 @@ class HomeViewModel(
     private val ipDetailsDao: IpDetailsDao,
 ) : ViewModel() {
 
-    private val _ipDetail: MutableLiveData<IpDetailRS> = MutableLiveData()
+    private val _ipDetail: MutableLiveData<IpDetailRS?> = MutableLiveData()
     val ipDetails = _ipDetail.readOnly()
 
     private val _errorLiveData: MutableLiveData<String> = MutableLiveData()
@@ -25,26 +27,47 @@ class HomeViewModel(
     fun getIpDetails(ipAddress: String = "") {
         viewModelScope.launch {
             if (ipAddress.isEmpty()) {
-                val response = publicIpService.getPublicIp()
-                if (response.isSuccessful) {
-                    _ipDetail.value = retrieveIpDetails(response.body()?.ip.orEmpty())
-                } else {
-                    _errorLiveData.value = "Could not fetch your public IP."
-                }
+                safeCall(
+                    {
+                        val response = publicIpService.getPublicIp()
+                        if (response.isSuccessful) {
+                            _ipDetail.value = retrieveIpDetails(response.body()?.ip.orEmpty())
+                        } else {
+                            _errorLiveData.value = "Could not fetch your public IP."
+                        }
+                    }, {
+                        _errorLiveData.value = it.localizedMessage
+                    }
+                )
             } else {
                 _ipDetail.value = retrieveIpDetails(ipAddress)
             }
         }
     }
 
-    private suspend fun retrieveIpDetails(ipAddress: String): IpDetailRS {
+    private suspend fun retrieveIpDetails(ipAddress: String): IpDetailRS? {
         return ipDetailsDao.getCachedIpAddress(ipAddress)?.toIpDetailRS()
             ?: getIpDetailsFromServer(ipAddress)
     }
 
-    private suspend fun getIpDetailsFromServer(ipAddress: String): IpDetailRS {
-        val ipDetails = ipScannerService.getIpDetails(ipAddress)
-        ipDetailsDao.saveIpDetails(ipDetails.toIpDetailsEntity())
-        return ipDetails
+    private suspend fun getIpDetailsFromServer(ipAddress: String): IpDetailRS? {
+        return safeCall(
+            {
+                val response = ipScannerService.getIpDetails(ipAddress)
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        delay(250L)
+                        ipDetailsDao.saveIpDetails(it.toIpDetailsEntity())
+                        it
+                    }
+                } else {
+                    _errorLiveData.value =
+                        "Could not fetch details. Please try again after a moment."
+                    null
+                }
+            }, {
+                _errorLiveData.value = it.localizedMessage
+            }
+        )
     }
 }
